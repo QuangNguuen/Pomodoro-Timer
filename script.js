1207a826be8800fe09186e61833f6eaa81cb2495
Quang <quangnghia0412@gmail.com>
2026-08-09 04:40:38 -0600

    Mostly done, basic controls are finished. Need a bit time for setting section

--- script.js
+++ script.js
@@ -1,95 +1,124 @@
-//Time options//
-const pomodoro = document.getElementById('pomodoro');
-const short = document.getElementById('short');
-const long = document.getElementById('long');
+    //Any extra variable//
+    let isRunning = false;
+    const clock = document.getElementById('clock');
+    //Time options//
+    const pomodoro = document.getElementById('pomodoro');
+    const short = document.getElementById('short');
+    const long = document.getElementById('long');
 
 
-//Start-Pause-Restart//
-const start = document.getElementById('start');
-const restart = document.getElementById('restart');
+    //Start-Pause-Restart//
+    const start = document.getElementById('start');
+    const restart = document.getElementById('restart');
 
-//Times in the clock//
-const setTimes = {
-    pomodoro: 25*60,
-    short: 5*60,
-    long: 10*60,
-}
+    //Times in the clock//
+    const setTimes = {
+        pomodoro: 25*60,
+        short: 5*60,
+        long: 10*60,
+    }
 
-let timeLeft = setTimes.pomodoro; //The standard time//
+    let timeLeft = setTimes.pomodoro; //The standard time//
 
-//Format for the clock//
-function format() { //makes sure the format always work, no matter the time//
-    const minute = Math.floor(timeLeft/60);
-    const second = timeLeft%60;
+    //Format for the clock//
+    function format() { //makes sure the format always work, no matter the time//
+        const minute = Math.floor(timeLeft/60);
+        const second = timeLeft%60;
 
-    const formatSecond = String(second).padStart(2,'0');
-    clock.textContent = `${minute}:${formatSecond}`;
-}
-format(); //Default time when the application is open//
+        const formatSecond = String(second).padStart(2,'0');
+        clock.textContent = `${minute}:${formatSecond}`;
+    }
+    format(); //Default time when the application is open//
 
-pomodoro.addEventListener('click', function() {  //Timer when any buttons clicked//
-    timeLeft = setTimes.pomodoro;
-    format();
-});
-short.addEventListener('click', function() {
-    timeLeft = setTimes.short;
-    format();
-});
-long.addEventListener('click', function() {
-    timeLeft = setTimes.long;
-    format();
-});
-
-
-//Countdown//
-let timerID;
-let isRunning = false;
-function countdown() {
-    format();
-    timerID = setInterval(function() {
-        timeLeft--;
+    let totalTime = setTimes.pomodoro;
+    //Simple time variables//
+    pomodoro.addEventListener('click', function() {  //Timer when any buttons clicked//
+        timeLeft = setTimes.pomodoro;
+        totalTime = setTimes.pomodoro;
         format();
+    });
+    short.addEventListener('click', function() {
+        timeLeft = setTimes.short;
+        totalTime = setTimes.short;
+        format();
+    });
+    long.addEventListener('click', function() {
+        timeLeft = setTimes.long;
+        totalTime = setTimes.long;
+        format();
+    })
 
-        if (timeLeft <= 0) {
-            clearInterval(timerID);
-            isRunning = false;
-            start.textContent = 'start';
+
+    //Countdown//
+    let timerID;
+    function countdown() {
+        format();
+        timerID = setInterval(function() {
+            timeLeft--;
+            format();
+
+            if (timeLeft <= 0) {
+                clearInterval(timerID);
+                isRunning = false;
+                start.textContent = 'start';
+            }
+
+        },1000);
+    }
+
+    start.addEventListener('click', function() {
+        if (!isRunning) {
+            format();
+            countdown();
+            start.textContent = 'pause';
+            isRunning = true;
         }
 
-    },1000);
-}
+        else {
+            isRunning = false;
+            clearInterval(timerID);
+            start.textContent = 'start';
+        }
+    });
 
-start.addEventListener('click', function() {
-    if (!isRunning) {
-        format();
-        countdown();
-        start.textContent = 'pause';
-        isRunning = true;
-    }
-
-    else {
-        isRunning = false;
+    //Restart the clock//
+    //Stop the clock and stop at the exact starting time//
+    restart.addEventListener('click', function() {
         clearInterval(timerID);
-        start.textContent = 'start';
-    }
-});
-
-//Restart the clock//
-//Stop the clock and stop at the exact starting time//
-function formatForRestart() {
-    const minute = Math.floor(setTimes.pomodoro/60);
-    const second = setTimes.pomodoro%60;
-
-    const formatSecond = String(second).padStart(2, '0');
-    clock.textContent = `${minute}:${formatSecond}`;
-}
-
-restart.addEventListener('click', function() {
-    if (!isRunning) {
-        formatForRestart();
-        isRunning = true;
-    }
-    else {
         isRunning = false;
-    }
-});
+        
+        timeLeft = totalTime;
+        format();
+
+        start.textContent = 'start';
+    });
+    
+    pomodoro.addEventListener('click', function() {
+        clearInterval(timerID);
+        isRunning = false;
+
+        timeLeft = totalTime;
+        format();
+
+        start.textContent = 'start';
+    });
+
+    short.addEventListener('click', function() {
+        clearInterval(timerID);
+        isRunning = false;
+
+        timeLeft = totalTime;
+        format();
+
+        start.textContent = 'start';
+    });
+
+    long.addEventListener('click', function() {
+        clearInterval(timerID);
+        isRunning = false;
+
+        timeLeft = totalTime;
+        format();
+
+        start.textContent = 'start';
+    });
